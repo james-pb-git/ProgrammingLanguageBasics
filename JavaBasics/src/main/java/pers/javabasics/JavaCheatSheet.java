d694088e347047829c2003e9670e469c813e008a
James <jamesjiongsi@gmail.com>
2018-09-30 11:46:15 +0800

    Init Java cheat sheet.

---
+++ JavaBasics/src/main/java/pers/javabasics/JavaCheatSheet.java
@@ -0,0 +1,10 @@
+package main.java.pers.javabasics;
+
+/**
+ * Created by bopang on 9/30/18.
+ */
+public class JavaCheatSheet {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
