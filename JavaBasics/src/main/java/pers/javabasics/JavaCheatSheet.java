cea455139dc318fc1d5ebebdfeba88336db6a6d7
James <jamesjiongsi@gmail.com>
2019-02-04 10:21:16 -0500

    numpy basics notebook init.

--- JavaBasics/src/main/java/pers/javabasics/JavaCheatSheet.java
+++ JavaBasics/src/main/java/pers/javabasics/JavaCheatSheet.java
@@ -4,6 +4,17 @@ package pers.javabasics;
  * Created by bopang on 9/30/18.
  */
 public class JavaCheatSheet {
+
+    public void foo(int num) {
+        if(num > 0) {
+            int result = 10;
+        }
+        else {
+            int result = 20;
+        }
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
